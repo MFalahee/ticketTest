893464c2dc483249006834f1ce56d3ea446d2ee0
mjfalahee <mjfalahee@gmail.com>
2022-08-15 17:20:58 -0700

    reverted sc

--- src/components/SoundCloudWidget.tsx
+++ src/components/SoundCloudWidget.tsx
@@ -11,7 +11,7 @@ export default function SoundCloudWidget() {
         scrolling="no"
         frameBorder="no"
         allow="autoplay"
-        src="https://w.soundcloud.com/player/?url=https://api.soundcloud.com/users/10915053/sets/heavy-glow-tour-vips/s-8DtQOIkYrrS?si=a79f6faf75144bf1974c4c8a6cf38d75"
+        src="https://w.soundcloud.com/player/?url=https://api.soundcloud.com/users/10915053"
       />
     </div>
 )}
