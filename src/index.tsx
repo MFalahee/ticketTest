f988a8972f492eddf7a36657058e299a52e5de66
mjfalahee <mjfalahee@gmail.com>
2022-09-09 15:28:53 -0700

    router error on deployed version, trying to figure out what I did wrong

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,27 @@ import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
-
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='Glow'>
+        <Route path='glow'>
           <Route path=':city' element={<App />} />
         </Route>
+        <Route
+          path='/'
+          element={
+            <div>
+              <p> Default page for Heavy Glow Tour</p>
+              <img
+                src={"./public/phlogo.png"}
+                alt={
+                  "elephante logo art that is basically a squiggly looking elephant - the dev"
+                }
+              />
+            </div>
+          }
+        ></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
