e68d4d48d223b09122603ed741b140c60cb99f6c
mjfalahee <mjfalahee@gmail.com>
2022-09-23 14:17:58 -0700

    small cleanup still

--- src/files/sectionsData.ts
+++ src/files/sectionsData.ts
@@ -1,21 +1,8 @@
 import { Section } from "./customTypes"
-
 let sections: Array<Section> = [
   {
     name: "Photos",
     subcategories: ["Relive Memories"],
-    photos: [
-      "/files/photos/conc1comp.png",
-      "/files/photos/conc2comp.png",
-      "/files/photos/conc3comp.png",
-      "/files/photos/conc4comp.png",
-      "/files/photos/conc5comp.png",
-      "/files/photos/conc6comp.png",
-      "/files/photos/conc7comp.png",
-      "/files/photos/conc8comp.png",
-      "/files/photos/conc9comp.png",
-      "/files/photos/conc10comp.png",
-    ],
     description: "Section to display photos for the specifc NFT ticket",
     id: 0,
   },
@@ -36,7 +23,6 @@ let sections: Array<Section> = [
   },
   {
     name: "Worlds",
-    photos: [],
     subcategories: ["Join us"],
     // Promote community and other NFT ticket websites potentially? Could incorporate 3d model if we want to be ambitious?
     description: "Section to display social media and discord channels.",
